23f9ab792f54bd4b62ed285422a44f5737ef6199
MySlow42 <46521577+MySlow42@users.noreply.github.com>
2023-05-30 16:25:02 +0200

    RCO - simplification

--- src/main/java/org/example/Day3.java
+++ src/main/java/org/example/Day3.java
@@ -1,13 +1,36 @@
 package org.example;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Scanner;
 import java.util.stream.Collectors;
 public class Day3 {
 
     private List<List<String>> groups = new ArrayList<>();
     private ArrayList<Character> itemByGroup = new ArrayList<>();
     private ArrayList<Character> itemsIdentiques = new ArrayList<>();
+
+    public static List<String> getInputByLine(String pathname) {
+        List<String> backpacks = new ArrayList<>();
+        try {
+            File input = new File(pathname);
+            Scanner scanner = new Scanner(input);
+
+            while (scanner.hasNextLine()) {
+                String backpack = scanner.nextLine();
+                backpacks.add(backpack);
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Fichier non trouvé.");
+            e.printStackTrace();
+        }
+        return backpacks;
+    }
 
     public Integer resolvePuzzlePart1(List<String> backpacks) {
         for (String s : backpacks) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Persistance data = new Persistance();
         Day3 day3 = new Day3();
         System.out.println("Day3");
-        List<String> Backpacks = data.getInputByLine("src\\main\\java\\org\\example\\inputDay3.txt");
-        System.out.println(day3.resolvePuzzlePart1(Backpacks));
-        System.out.println(day3.resolvePuzzlePart2(Backpacks) + "\n");
+        List<String> Backpacks = Day3.getInputByLine("src\\main\\java\\org\\example\\inputDay3.txt");
+        System.out.println("La première somme recherchée est: "  + day3.resolvePuzzlePart1(Backpacks));
+        System.out.println("La seconde somme recherchée est: "+day3.resolvePuzzlePart2(Backpacks) + "\n");
 
     }
 }
